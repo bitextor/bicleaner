d21c1ff6feb32aaa6ad7002755e30c71dfece268
Marta Bañón <mbanon@prompsit.com>
2018-09-28 15:42:48 +0200

    Added setup and teardown methods to testset

--- tests/bicleaner_test.py
+++ tests/bicleaner_test.py
@@ -4,23 +4,32 @@ __version__ = "Version 0.1 # 28/09/2018 # Classifier test # Marta Bañón"
 import subprocess
 #from subprocess import check_output
 
-
-def bicleaner_test():
+def setup_function():
 	langpackurl = "https://github.com/bitextor/bitextor-data/raw/master/bicleaner/en-de.tar.gz"
 	tar = "tar -xzf en-de.tar.gz"
+	command = "cd src && mkdir -p lang && cd lang && wget -q {0} && {1}  && cd ../..".format(langpackurl, tar)	
+	p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+	p.wait()
+
+def teardown_function():
+	command = "rm -r src/lang && rm tests/test-corpus.en-de.classified"
+	p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+	p.wait()
+
+def bicleaner_test():
+
 	bicleaner_cmd = "python3 src/bicleaner-classifier-full.py \
       tests/test-corpus.en-de  \
       tests/test-corpus.en-de.classified  \
       -m src/lang/en-de/training.en-de.yaml \
       -b 100  -q"
-	awk = "awk -F'\t' '$5 > 0 {print ;}' tests/test-corpus.en-de.classified"     
+#	awk = "awk -F'\t' '$5 > 0 {print ;}' tests/test-corpus.en-de.classified"     
 
-	command = "cd src && mkdir -p lang && cd lang && wget -q {0} && {1}  && cd ../.. && {2}".format(langpackurl, tar, bicleaner_cmd)
-	p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE)
+	p = subprocess.Popen(bicleaner_cmd, shell=True, stdout=subprocess.PIPE)
 	p.wait()
 
-	passed = 0
-
+	current = 0
+	passed = 0	
 	with open("tests/test-corpus.en-de.classified", "r") as classified_file:
 		for line in classified_file:	
 			try:
