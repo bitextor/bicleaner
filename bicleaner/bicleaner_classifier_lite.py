e06d4e83f627fca6f4e2d9730f8b9f3fa88904ec
Faheem Kirefu <fkirefu@ed.ac.uk>
2019-09-06 15:39:43 +0100

    added score_only function and made compatible with cache

--- bicleaner/bicleaner_classifier_lite.py
+++ bicleaner/bicleaner_classifier_lite.py
@@ -47,9 +47,9 @@ def initialization():
     parser = argparse.ArgumentParser(prog=os.path.basename(sys.argv[0]), formatter_class=argparse.ArgumentDefaultsHelpFormatter, description=__doc__)
     # Mandatory parameters
     ## Input file. Try to open it to check if it exists
-    parser.add_argument('input', type=argparse.FileType('rt'), default=None, help="Tab-separated files to be classified")      
+    parser.add_argument('input', type=argparse.FileType('rt'), default=None, help="Tab-separated files to be classified")
     parser.add_argument('output', nargs='?', type=argparse.FileType('w'), default=sys.stdout, help="Output of the classification")
-    parser.add_argument('metadata', type=argparse.FileType('r'), default=None, help="Training metadata (YAML file)")    
+    parser.add_argument('metadata', type=argparse.FileType('r'), default=None, help="Training metadata (YAML file)")
 
     # Options group
     groupO = parser.add_argument_group('Optional')
@@ -60,7 +60,8 @@ def initialization():
     groupO.add_argument('--threshold', type=check_positive_between_zero_and_one, default=0.5, help="Threshold for classifier. If accuracy histogram is present in metadata, the interval for max value will be given as a default instead the current default.")
     groupO.add_argument('--lm_threshold',type=check_positive_between_zero_and_one, default=0.5, help="Threshold for language model fluency scoring. All TUs whose LM fluency score falls below the threshold will are removed (classifier score set to 0), unless the option --keep_lm_result set.")
     groupO.add_argument('--keep_lm_result',action='store_true', help="Add an additional column to the results with the language model fluency score and do not discard any TU based on that score.")
-    
+    groupO.add_argument('--score_only',action='store_true', help="Only output one column which is the bicleaner score", default=False)
+
     # Logging group
     groupL = parser.add_argument_group('Logging')
     groupL.add_argument('-q', '--quiet', action='store_true', help='Silent logging mode')
@@ -72,11 +73,12 @@ def initialization():
     # Checking if metadata is specified
     args = parser.parse_args()
     logging_setup(args)
-    
-    try: 
+
+
+    try:
         yamlpath = os.path.dirname(os.path.abspath(args.metadata.name))
 
-        metadata_yaml = yaml.load(args.metadata)      
+        metadata_yaml = yaml.load(args.metadata)
 
         args.source_lang=metadata_yaml["source_lang"]
         args.target_lang=metadata_yaml["target_lang"]
@@ -87,23 +89,23 @@ def initialization():
 
         try:
             args.clf=joblib.load( os.path.join( yamlpath , metadata_yaml["classifier"]))
-        except:            
+        except:
             args.clf=joblib.load(metadata_yaml["classifier"])
-        
-#        args.clf.n_jobs = None    
+
+#        args.clf.n_jobs = None
         args.classifier_type=metadata_yaml["classifier_type"]
 
 
         try:
             args.dict_sl_tl = ProbabilisticDictionary( os.path.join(yamlpath , metadata_yaml["source_dictionary"]))
         except:
-            args.dict_sl_tl = ProbabilisticDictionary(metadata_yaml["source_dictionary"])                
-        try:            
-            args.dict_tl_sl = ProbabilisticDictionary( os.path.join(yamlpath , metadata_yaml["target_dictionary"]))        
+            args.dict_sl_tl = ProbabilisticDictionary(metadata_yaml["source_dictionary"])
+        try:
+            args.dict_tl_sl = ProbabilisticDictionary( os.path.join(yamlpath , metadata_yaml["target_dictionary"]))
         except:
-            args.dict_tl_sl = ProbabilisticDictionary(metadata_yaml["target_dictionary"])        
-        
-                
+            args.dict_tl_sl = ProbabilisticDictionary(metadata_yaml["target_dictionary"])
+
+
         args.normalize_by_length = metadata_yaml["normalize_by_length"]
         args.treat_oovs = metadata_yaml["treat_oovs"]
         args.qmax_limit = metadata_yaml["qmax_limit"]
@@ -114,23 +116,23 @@ def initialization():
         args.wrong_test_examples = metadata_yaml["wrong_test_examples"]
         args.length_ratio = metadata_yaml["length_ratio"]
         args.features_version = 1 if "features_version" not in metadata_yaml else int(metadata_yaml["features_version"])
-        
+
         threshold = np.argmax(metadata_yaml["accuracy_histogram"])*0.1
         logging.info("Accuracy histogram: {}".format(metadata_yaml["accuracy_histogram"]))
         logging.info("Ideal threshold: {:1.1f}".format(threshold))
         metadata_yaml["threshold"] = threshold
-        
-        #Load LM stuff if model was trained with it 
+
+        #Load LM stuff if model was trained with it
         if "source_lm" in metadata_yaml and "target_lm" in metadata_yaml:
             lmFilter = DualLMFluencyFilter( LMType[metadata_yaml['lm_type']] ,args.source_lang, args.target_lang)
             stats=DualLMStats( metadata_yaml['clean_mean_perp'],metadata_yaml['clean_stddev_perp'],metadata_yaml['noisy_mean_perp'],metadata_yaml['noisy_stddev_perp'] )
-            
+
             fullpath_source_lm=os.path.join(yamlpath,metadata_yaml['source_lm'])
             if os.path.isfile(fullpath_source_lm):
                 source_lm= fullpath_source_lm
             else:
                 source_lm= metadata_yaml['source_lm']
-            
+
             fullpath_target_lm=os.path.join(yamlpath,metadata_yaml['target_lm'])
             if os.path.isfile(fullpath_target_lm):
                 target_lm=fullpath_target_lm
@@ -140,19 +142,24 @@ def initialization():
             args.lm_filter=lmFilter
         else:
             args.lm_filter=None
-        
+
         logging.debug("YAML")
         logging.debug(metadata_yaml)
-        parser.set_defaults(**metadata_yaml)   
-   
+        parser.set_defaults(**metadata_yaml)
+
     except:
         print("Error loading metadata")
         traceback.print_exc()
         sys.exit(1)
-    
+
     # Ensure that directory exists; if not, create it
     if not os.path.exists(args.tmp_dir):
         os.makedirs(args.tmp_dir)
+
+
+    if args.score_only and args.keep_lm_result:
+        raise AssertionError("Conflicting arguments, cannot output bicleaner score only AND keep language model result!")
+
 
     logging.debug("Arguments processed: {}".format(str(args)))
     logging.info("Arguments processed.")
@@ -177,13 +184,17 @@ def classify(args):
     for i in args.input:
         nline += 1
         parts = i.split("\t")
-        
+
         sl_sentence=None
         tl_sentence=None
         if len(parts) >= 4:
             sl_sentence=parts[2]
             tl_sentence=parts[3]
-       
+
+        if len(parts) == 2:
+            sl_sentence=parts[0]
+            tl_sentence=parts[1]
+
         if sl_sentence and tl_sentence and len(sl_sentence.strip()) != 0 and len(tl_sentence.strip()) != 0 and wrong_tu(sl_sentence.strip(),tl_sentence.strip(), args)== False:
             lmScore=None
             if args.lm_filter:
@@ -199,26 +210,36 @@ def classify(args):
             if args.lm_filter:
                 lmScore=0
             buf_sent.append((0, i, lmScore))
-        
+
         if (nline % batch_size) == 0:
             args.clf.set_params(n_jobs = 1)
             predictions = args.clf.predict_proba(np.array(buf_feat)) if len(buf_feat) > 0 else []
             p = iter(predictions)
-                
+
             for k, l, lmScore in buf_sent:
                 if k == 1:
-                    args.output.write(l.strip())
-                    args.output.write("\t")
-                    args.output.write(str(next(p)[1]))
-                    if lmScore != None and args.keep_lm_result:
+
+                    if args.score_only:
+                        args.output.write(str(next(p)[1]))
+                    else:
+
+                        args.output.write(l.strip())
                         args.output.write("\t")
-                        args.output.write(str(lmScore))
+                        args.output.write(str(next(p)[1]))
+                        if lmScore != None and args.keep_lm_result:
+                            args.output.write("\t")
+                            args.output.write(str(lmScore))
                     args.output.write("\n")
                 else:
-                    args.output.write(l.strip("\n"))
-                    args.output.write("\t0")
-                    if lmScore != None and args.keep_lm_result:
+
+                    if args.score_only:
+                        args.output.write("0")
+
+                    else:
+                        args.output.write(l.strip("\n"))
                         args.output.write("\t0")
+                        if lmScore != None and args.keep_lm_result:
+                            args.output.write("\t0")
                     args.output.write("\n")
 
             buf_feat = []
@@ -227,30 +248,40 @@ def classify(args):
     if len(buf_sent) > 0:
         predictions = args.clf.predict_proba(np.array(buf_feat)) if len(buf_feat) > 0 else []
         p = iter(predictions)
-            
+
         for k, l, lmScore in buf_sent:
             if k == 1:
-                args.output.write(l.strip())
-                args.output.write("\t")
-                args.output.write(str(next(p)[1]))
-                if lmScore != None and args.keep_lm_result:
+
+                if args.score_only:
+                    args.output.write(str(next(p)[1]))
+                else:
+
+                    args.output.write(l.strip())
                     args.output.write("\t")
-                    args.output.write(str(lmScore))
+                    args.output.write(str(next(p)[1]))
+                    if lmScore != None and args.keep_lm_result:
+                        args.output.write("\t")
+                        args.output.write(str(lmScore))
                 args.output.write("\n")
             else:
-                args.output.write(l.strip("\n"))
-                args.output.write("\t0")
-                if lmScore != None and args.keep_lm_result:
+
+                if args.score_only:
+                    args.output.write("0")
+
+                else:
+                    args.output.write(l.strip("\n"))
                     args.output.write("\t0")
+                    if lmScore != None and args.keep_lm_result:
+                        args.output.write("\t0")
                 args.output.write("\n")
-                
-                
+
+
 # Filtering input texts
 def perform_classification(args):
     global nline
     time_start = default_timer()
     logging.info("Starting process")
-    
+
     classify(args)
 
     # Stats
