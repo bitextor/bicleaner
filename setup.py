fcd0e9671f59c9da41a84faba7e880f32384173a
Tudor Nicolae Mateiu <tudormateiu321@gmail.com>
2022-12-19 10:07:35 +0000

    Updated requirements

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open("requirements.txt") as rf:
     
 setuptools.setup(
     name="bicleaner",
-    version="0.15.3",
+    version="0.16",
     install_requires=requirements,
     python_requires='>=3.7',
     license="GNU General Public License v3.0",
